e575ede48eb2af97b7095b6228c8e90278db866d
Saulo Machado <saulo.machado@g.globo>
2022-09-11 00:48:37 -0300

    Creating physics material to remove clicping to wall by zeroing drag; Adding motion drag to decrease speed on movement script; Adding jump; Checking if the player is ground and therefor able to jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,17 +4,59 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    [Header("Controls")]
+    public KeyCode left = KeyCode.LeftArrow;
+    public KeyCode right = KeyCode.LeftArrow;
+    public KeyCode jump = KeyCode.Space;
+
+    [Header("Movement Configruations")]
+    public float speed = 3f;
+    public float jumpForce = 8f;
+    public Vector2 friction = new Vector2(0.1f, 0);
 
     public Rigidbody2D rigidBody;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow)) {
+        HandleMovement();
+        HandleJump();
+        DecreaseSpeed();
+        var startPosition = transform.position;
+        startPosition.y -= GetComponent<BoxCollider2D>().bounds.extents.y;
+        Debug.DrawRay(startPosition, Vector2.down * 0.1f, Color.red);
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(left))
+        {
             rigidBody.velocity = new Vector2(-speed, rigidBody.velocity.y);
-        } else if (Input.GetKey(KeyCode.RightArrow)) {
+        }
+        else if (Input.GetKey(right))
+        {
             rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
         }
+    }
+
+    private void HandleJump() {
+        if (Input.GetKeyDown(jump) && IsGrounded())
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        }
+    }
+
+    private void DecreaseSpeed() {
+        if (rigidBody.velocity.x == 0) return;
+        int signal = rigidBody.velocity.x > 0 ? -1 : 1;
+        rigidBody.velocity = (Mathf.Abs(rigidBody.velocity.x) < friction.x) 
+            ? new Vector2(0, rigidBody.velocity.y) 
+            : new Vector2(rigidBody.velocity.x + friction.x * signal, rigidBody.velocity.y);
+    }
+
+    private bool IsGrounded() {
+        var startPosition = transform.position;
+        startPosition.y -= GetComponent<BoxCollider2D>().bounds.extents.y;
+        return Physics2D.Raycast(startPosition, Vector2.down, 0.1f);
     }
 }
